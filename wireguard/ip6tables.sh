05a0ad4d95c469e197241b73916fbda8a5a626ed
Pranav Darshan <65911046+PranavDarshan@users.noreply.github.com>
2025-07-16 15:22:37 +0530

    Create ip6tables.sh

---
+++ wireguard/ip6tables.sh
@@ -0,0 +1,25 @@
+# Flush existing rules
+sudo ip6tables -F
+sudo ip6tables -X
+
+# Set default policies
+sudo ip6tables -P INPUT DROP
+sudo ip6tables -P FORWARD DROP
+sudo ip6tables -P OUTPUT ACCEPT
+
+# Allow loopback and established/related connections
+sudo ip6tables -A INPUT -i lo -j ACCEPT
+sudo ip6tables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allow SSH over IPv6
+sudo ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Allow WireGuard over IPv6 (UDP 51820)
+sudo ip6tables -A INPUT -p udp --dport 51820 -j ACCEPT
+
+# Allow HTTP (port 80) only from local IPv6 network (ULA example fd00::/8)
+sudo ip6tables -A INPUT -p tcp --dport 80 -s fd00::/8 -j ACCEPT
+
+# Allow VPN client forwarding
+sudo ip6tables -A FORWARD -i wg0 -o wlp11s0 -j ACCEPT
+sudo ip6tables -A FORWARD -i wlp11s0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
