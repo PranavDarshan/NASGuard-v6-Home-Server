78178dcfbb46dc9642e99a441f87cfacf63d612c
Pranav Darshan <65911046+PranavDarshan@users.noreply.github.com>
2025-07-16 15:43:59 +0530

    Update iptables.sh

--- wireguard/iptables.sh
+++ wireguard/iptables.sh
@@ -15,20 +15,30 @@ sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 # Allow SSH
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-# Allow WireGuard
+# Allow WireGuard (UDP 51820)
 sudo iptables -A INPUT -p udp --dport 51820 -j ACCEPT
 
-# Allow HTTP (port 80) only from local network
-sudo iptables -A INPUT -p tcp --dport 80 -s 192.168.29.0/8 -j ACCEPT
+# Allow HTTP from LAN only
+sudo iptables -A INPUT -p tcp --dport 80 -s 192.168.29.0/24 -j ACCEPT
 
-# WireGuard forwarding rules
+# Allow DNS (UDP + TCP 53) from LAN
+sudo iptables -A INPUT -p udp --dport 53 -s 192.168.29.0/24 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 53 -s 192.168.29.0/24 -j ACCEPT
+
+# Allow DNS from WireGuard clients
+sudo iptables -A INPUT -i wg0 -p udp --dport 53 -j ACCEPT
+sudo iptables -A INPUT -i wg0 -p tcp --dport 53 -j ACCEPT
+
+# Allow AdGuard Web UI (port 3000) from LAN
+sudo iptables -A INPUT -p tcp --dport 3000 -s 192.168.29.0/24 -j ACCEPT
+
+# Allow Samba from WireGuard clients
+sudo iptables -A INPUT -i wg0 -p tcp -m multiport --dports 139,445 -j ACCEPT
+sudo iptables -A INPUT -i wg0 -p udp -m multiport --dports 137,138 -j ACCEPT
+
+# Allow forwarding for WireGuard VPN clients
 sudo iptables -A FORWARD -i wg0 -o wlp11s0 -j ACCEPT
 sudo iptables -A FORWARD -i wlp11s0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 # NAT for VPN clients
 sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o wlp11s0 -j MASQUERADE
-
-# Allow Samba ports from WireGuard clients
-sudo iptables -A INPUT -i wg0 -p tcp -m multiport --dports 139,445 -j ACCEPT
-sudo iptables -A INPUT -i wg0 -p udp -m multiport --dports 137,138 -j ACCEPT
-
