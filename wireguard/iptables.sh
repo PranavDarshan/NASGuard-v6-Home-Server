f841065d02ed4a3c61d5cf59a64a10c5456820f3
Pranav Darshan <65911046+PranavDarshan@users.noreply.github.com>
2025-07-16 15:44:41 +0530

    Update iptables.sh

--- wireguard/iptables.sh
+++ wireguard/iptables.sh
@@ -1,44 +1,44 @@
-# Flush old rules
-sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -X
+# Flush existing rules
+sudo ip6tables -F
+sudo ip6tables -X
 
 # Default policies
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
-sudo iptables -P OUTPUT ACCEPT
+sudo ip6tables -P INPUT DROP
+sudo ip6tables -P FORWARD DROP
+sudo ip6tables -P OUTPUT ACCEPT
 
-# Loopback and related traffic
-sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+# Loopback and related connections
+sudo ip6tables -A INPUT -i lo -j ACCEPT
+sudo ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Allow SSH
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+# Essential ICMPv6
+sudo ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
+sudo ip6tables -A FORWARD -p ipv6-icmp -j ACCEPT
 
-# Allow WireGuard (UDP 51820)
-sudo iptables -A INPUT -p udp --dport 51820 -j ACCEPT
+# WireGuard
+sudo ip6tables -A INPUT -p udp --dport 51820 -j ACCEPT
 
-# Allow HTTP from LAN only
-sudo iptables -A INPUT -p tcp --dport 80 -s 192.168.29.0/24 -j ACCEPT
+# SSH
+sudo ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
 
-# Allow DNS (UDP + TCP 53) from LAN
-sudo iptables -A INPUT -p udp --dport 53 -s 192.168.29.0/24 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 53 -s 192.168.29.0/24 -j ACCEPT
+# HTTP from local IPv6 (ULA) subnet
+sudo ip6tables -A INPUT -p tcp --dport 80 -s fd00::/8 -j ACCEPT
 
-# Allow DNS from WireGuard clients
-sudo iptables -A INPUT -i wg0 -p udp --dport 53 -j ACCEPT
-sudo iptables -A INPUT -i wg0 -p tcp --dport 53 -j ACCEPT
+# DNS from LAN over IPv6
+sudo ip6tables -A INPUT -p udp --dport 53 -s fd00::/8 -j ACCEPT
+sudo ip6tables -A INPUT -p tcp --dport 53 -s fd00::/8 -j ACCEPT
 
-# Allow AdGuard Web UI (port 3000) from LAN
-sudo iptables -A INPUT -p tcp --dport 3000 -s 192.168.29.0/24 -j ACCEPT
+# DNS from WireGuard clients
+sudo ip6tables -A INPUT -i wg0 -p udp --dport 53 -j ACCEPT
+sudo ip6tables -A INPUT -i wg0 -p tcp --dport 53 -j ACCEPT
 
-# Allow Samba from WireGuard clients
-sudo iptables -A INPUT -i wg0 -p tcp -m multiport --dports 139,445 -j ACCEPT
-sudo iptables -A INPUT -i wg0 -p udp -m multiport --dports 137,138 -j ACCEPT
+# AdGuard Web UI from local ULA
+sudo ip6tables -A INPUT -p tcp --dport 3000 -s fd00::/8 -j ACCEPT
 
-# Allow forwarding for WireGuard VPN clients
-sudo iptables -A FORWARD -i wg0 -o wlp11s0 -j ACCEPT
-sudo iptables -A FORWARD -i wlp11s0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+# Samba from WireGuard clients
+sudo ip6tables -A INPUT -i wg0 -p tcp -m multiport --dports 139,445 -j ACCEPT
+sudo ip6tables -A INPUT -i wg0 -p udp -m multiport --dports 137,138 -j ACCEPT
 
-# NAT for VPN clients
-sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o wlp11s0 -j MASQUERADE
+# WireGuard client internet access
+sudo ip6tables -A FORWARD -i wg0 -o wlp11s0 -j ACCEPT
+sudo ip6tables -A FORWARD -i wlp11s0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
